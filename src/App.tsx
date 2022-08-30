aeb5994210c3f09e9bca6421a3aa9b8b7bba0bfa
rabbitandcat <2365895472@qq.com>
2022-08-30 20:06:35 +0800

    feat: component button basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
 import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Button, { ButtonType, ButtonSize } from './components/button/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Vite + React</h2>
-      <h3>Vite + React</h3>
-      <h4>Vite + React</h4>
-      <code>
-        const a = 'b'
-      </code>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button disabled>Hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+      <Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled>Baidu Link</Button>
     </div>
   )
 }
