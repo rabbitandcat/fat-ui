cc88330b4c1bdf90e56c074b9cd065ccf0351627
rabbitandcat <2365895472@qq.com>
2022-09-01 18:58:43 +0800

    feat: component menu init

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Button from './components/Button'
 import Alert from './components/Alert'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Alert title='defaultTitle' onClose={() => alert('haha')}></Alert>
+      <Menu defaultIndex={0}>
+        <MenuItem>
+          cool link
+        </MenuItem>
+        <MenuItem>
+          cool link 2
+        </MenuItem>
+        <MenuItem>
+          cool link 3
+        </MenuItem>
+      </Menu>
     </div>
   )
 }
