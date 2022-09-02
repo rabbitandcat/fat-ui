4dc9ce198cb51595fffa5a28699fdc5d91c109bd
rabbitandcat <2365895472@qq.com>
2022-09-02 11:53:57 +0800

    test: SubMenu component

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import Button from './components/Button'
 import Alert from './components/Alert'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
-        <MenuItem index={0}>
+      <Menu defaultIndex={'0'} onSelect={(index) => { alert(index) }}>
+        <MenuItem index={'0'}>
           cool link
         </MenuItem>
-        <MenuItem index={1} disabled>
+        <MenuItem index={'1'} disabled>
           cool link 2
         </MenuItem>
-        <MenuItem index={2}>
-          cool link 3
-        </MenuItem>
+        <SubMenu title='haha'>
+          <MenuItem index={'2'}>
+            cool link 3
+          </MenuItem>
+        </SubMenu>
       </Menu>
     </div>
   )
